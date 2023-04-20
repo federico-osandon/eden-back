3a18e6aa5b83523ef893771027903c6881518589
defe014@gmail.com <defe014@gmail.com>
2023-04-19 23:50:19 -0300

    actualizado dev

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const PORT = 8080 || process.env.PORT
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
 app.use(logger('dev'))
 
