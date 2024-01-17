169ccc15c3b0bf6f8a4a2cae5231e5f647c52482
Federico <defe014@gmail.com>
2024-01-17 12:39:51 +0100

    nuevo

--- src/server.js
+++ src/server.js
@@ -13,7 +13,10 @@ const { PORT, cookieSecret } = serverConfigObject
 /* These lines of code are configuring and setting up various middleware functions for the Express
 application. */
 app.disable('x-powered-by')
-app.use(cors())
+app.use(cors({
+    origin: ['https://65a7a97ae65e9a947c719974--effulgent-froyo-ad7d60.netlify.app', 'http://localhost:8080'],
+    credentials: true
+}))
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
